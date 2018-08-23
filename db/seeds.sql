09b4bc50926920e9fed4ec98538d198ba0a4a6d2
Colby Schumm <coolbreeze@Colbys-MacBook-Air.local>
2018-08-23 14:53:34 -0600

    added the css

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name,devoured) VALUES ("Fied Egg and Ham Burger", false);
+INSERT INTO burgers (burger_name,devoured) VALUES ("Peanut Butter Bacon Jalapeno Burger",false);
+INSERT INTO burgers (burger_name,devoured) VALUES ("Bacon Cheese Burger", true);
+INSERT INTO burgers (burger_name,devoured) VALUES ("Veggie Burger (crime angainst humanity",true);  
