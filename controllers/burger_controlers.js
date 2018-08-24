491c68965d2d118a692d7b646f6ec0f829fe1851
Colby Schumm <coolbreeze@Colbys-MacBook-Air.local>
2018-08-24 13:52:21 -0600

    I hate this hw assignment

--- controllers/burger_controlers.js
+++ controllers/burger_controlers.js
@@ -3,8 +3,8 @@ var express = require("express");
 var router = express.Router();
 
 // Import the model (burger.js) to use its database functions.
-// var burger = require("../models/burger.js");
-var burger = require("../public/js/feburger");
+var burger = require("../models/burger.js");
+// var burger = require("../public/js/feburger");
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
   burger.all(function(data) {
